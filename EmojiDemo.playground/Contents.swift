765e6c2b874cfd179f480ceac2e5f1d1efb5216f
Bruce Evans <bruce.evans@linecorp.com>
2018-10-10 14:43:04 +0900

    Demo NSTextAttribute

--- EmojiDemo.playground/Contents.swift
+++ EmojiDemo.playground/Contents.swift
@@ -30,5 +30,23 @@ let myViewController = MyViewController()
 PlaygroundPage.current.liveView = myViewController
 
 let attributedText = NSMutableAttributedString(string: "Hello Attributed-World")
-myViewController.updateLabel(attributedText: attributedText)
 
+/*
+// NSTextAttachment - The Easy Way
+let attachment = NSTextAttachment()
+let image = UIImage(named: "1048696.png")
+attachment.image = image
+let emojiString = NSAttributedString(attachment: attachment)
+attributedText.append(emojiString)
+myViewController.updateLabel(attributedText: attributedText)
+*/
+
+// NSTextAttachment - The Designated Way
+let url = Bundle.main.url(forResource: "1048696",
+                          withExtension: "png")!
+let data = try! Data.init(contentsOf: url, options: [])
+let attachment = NSTextAttachment(data: data,
+                                  ofType: "public.image")
+let emojiString = NSAttributedString(attachment: attachment)
+attributedText.append(emojiString)
+myViewController.updateLabel(attributedText: attributedText)
