54520527733c1ff4530b36fbe9749dc5d0b65fd4
Bruce Evans <bruce.evans@linecorp.com>
2018-10-29 13:07:22 +0900

    Demo Emoticon

--- EmojiDemo.playground/Contents.swift
+++ EmojiDemo.playground/Contents.swift
@@ -25,28 +25,38 @@ class MyViewController : UIViewController {
     }
 }
 
+extension String {
+    static let emoticonSet =
+        CharacterSet(charactersIn: UnicodeScalar(0x100000)!...UnicodeScalar(0x100100)!)
+
+    func showEmoji() -> NSAttributedString {
+        guard let range = self.rangeOfCharacter(from: String.emoticonSet) else {
+            return NSMutableAttributedString(string: self)
+        }
+
+        let emojiString =
+            NSMutableAttributedString(string:String(self[startIndex..<range.lowerBound]))
+
+        let emojiId: UInt32 = self[range].unicodeScalars.first!.value
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(named: "\(emojiId)")
+        emojiString.append(NSAttributedString(attachment: attachment))
+
+        let substring = String(self[range.upperBound..<self.endIndex])
+        emojiString.append(substring.showEmoji())
+
+        return emojiString
+    }
+}
+
 // Present the view controller in the Live View window
 let myViewController = MyViewController()
 PlaygroundPage.current.liveView = myViewController
 
 let attributedText = NSMutableAttributedString(string: "Hello Attributed-World")
 
-/*
-// NSTextAttachment - The Easy Way
-let attachment = NSTextAttachment()
-let image = UIImage(named: "1048696.png")
-attachment.image = image
-let emojiString = NSAttributedString(attachment: attachment)
-attributedText.append(emojiString)
-myViewController.updateLabel(attributedText: attributedText)
-*/
-
-// NSTextAttachment - The Designated Way
-let url = Bundle.main.url(forResource: "1048696",
-                          withExtension: "png")!
-let data = try! Data.init(contentsOf: url, options: [])
-let attachment = NSTextAttachment(data: data,
-                                  ofType: "public.image")
-let emojiString = NSAttributedString(attachment: attachment)
-attributedText.append(emojiString)
+// Show an Emoticon
+let emoticonUnicode = UnicodeScalar(0x100078)!
+let emoticonString = "\(emoticonUnicode)"
+attributedText.append(emoticonString.showEmoji())
 myViewController.updateLabel(attributedText: attributedText)
